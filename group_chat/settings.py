7f2f6c9cca4d796ba463f7d62029f412f50c5f62
Sapana Prajapati <sapana.prajapati2505@gmail.com>
2025-06-20 20:51:25 +0530

    Fix: Set LOGIN_URL to /accounts/login/

--- group_chat/settings.py
+++ group_chat/settings.py
@@ -130,7 +130,7 @@ import os
 TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'templates')]
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-LOGIN_URL = '/login/'
+LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
 
